b8b5f6ea996334e35e2c52ed1e44ff399454b832
wycliffepepela <wycliffepepelacrash@gmail.com>
2020-11-19 17:34:50 +0300

    first commit

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<a href="https://wycliffepepela.github.io/"  > Designed by : wycliffe</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
